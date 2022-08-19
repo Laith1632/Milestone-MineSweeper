e4d6c06a8b72164a905c51766afca50a509a6494
Laith Albusairi <laithalbusairi@gmail.com>
2022-08-19 03:47:11 -0700

    got board layout

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let width = 10
+let sqaures = []
+
+//create board
+funcation  createBoard() {}
+ for(let i - 0; i < width*width; i++) {
+    const square = document.createElement('div')
+    square.setAttribute('id', i)
+    grid.appendChild(square)
+ }
+
+
+
+
+
+})

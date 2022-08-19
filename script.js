b3bd67cc5f8d94f3219bbc0007748b420d65615c
Laith Albusairi <laithalbusairi@gmail.com>
2022-08-19 04:08:12 -0700

    random bomb generater and grid built

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 const grid = document.querySelector('.grid')
 let width = 10
+let bombAmount = 20
 let squares = []
 
 //create board
 function  createBoard() {
+    // get shuflled gmae array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb')
+    const emptyArray = Array(width*width - bombAmount).fill('valid')
+    console.log(bombsArray)
+    console.log(emptyArray)
+    const gameArray = emptyArray.concat(bombsArray)
+    console.log(gameArray)
+    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+    console.log(shuffledArray)
+
  for(let i = 0; i < width*width; i++) {
     const square = document.createElement('div')
     square.setAttribute('id', i)
